eb6219d4092a44a9c43e73fd44add2fddb16d566
satyam1407 <satyamthakur1407@gmail.com>
2025-09-14 00:09:07 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res)=> {
+    res.send('hello world')
+})
+
+app.listen(PORT, () => {
+    console.log(`the port is running at ${PORT}`)
+})
